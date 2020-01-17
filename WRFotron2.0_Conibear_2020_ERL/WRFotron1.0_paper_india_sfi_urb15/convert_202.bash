65d45bb7682c767329ed48c2122c436cd8968323
lukeconibear <earlacoa@leeds.ac.uk>
2020-01-17 11:11:10 +0000

    removing superfluous files

--- WRFotron2.0_Conibear_2020_ERL/WRFotron1.0_paper_india_sfi_urb15/convert_202.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -cwd -V                          # execute from current working directory and export all current environment variables to all spawned processes
-#$ -l h_rt=48:00:00                 # The wall clock time which is the amount of real time needed by the job
-#$ -pe ib 1                         # Specifies a job for parallel programs using MPI, nprocPre is the number of cores to be used by the parallel job
-#$ -l node_type=24core-128G
-#$ -l h_vmem=128G
-
-ncl 'file_in="wrfout_2014_chemopt202_jan.nc"' 'file_out="wrfout_2014_jan_chemopt202_cf.nc"' wrfout_to_cf_202.ncl
